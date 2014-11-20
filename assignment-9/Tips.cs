8980a2d1fae9827e5609c3f2de494d91e9950888
Doug <ddoner01@seattlecentral.edu>
2014-11-19 22:36:44 -0800

    Create Tips.cs

---
+++ assignment-9/Tips.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itc110_assignment9
+{
+    class Tip
+    {
+        //value fields
+        private const double tax = 0.094;
+        private double amount;
+        private double tipPercentage;
+
+        //returns value and sets value for private double amount
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        public double TipPercentage
+        {
+            get { return tipPercentage; }
+            set
+            {
+                if (value < 1)
+                {
+                    tipPercentage = value;
+                }
+                else
+                {
+                    tipPercentage = value / 100;
+                }
+            }
+        }
+
+        //public class methods
+
+        //returns the tip as a double
+        public double CalculateTip()
+        {
+            return Amount * TipPercentage;
+        }
+
+        //returns the tax as a double
+        public double CalculateTax()
+        {
+            return Amount * tax;
+        }
+
+        //returns a value, using the calculate tax and tip methods to create a total
+        public double CalculateTotal()
+        {
+            return Amount + CalculateTip() + CalculateTax();
+        }
+
+    }
+}
