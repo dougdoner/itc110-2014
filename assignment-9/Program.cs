6f67ab2301b9f14b8283671bb14583b0a42deb8b
Doug <ddoner01@seattlecentral.edu>
2014-11-19 22:36:07 -0800

    Create Program.cs

---
+++ assignment-9/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itc110_assignment9
+{
+    /// <summary>
+    /// This is for itc 110
+    /// Assignment-9
+    /// Taking the previous tip program, and adding a Write to file class.
+    /// The program will take the outputs of the tip class and write them to a file.
+    /// 
+    /// The Read file program will be a seperate console program, which will read the text files.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display disp = new Display();
+        }
+    }
+}
